bc8040c7c7a5222b61b6b004c19c9201d258d4ad
Rob <raven@hydenix.dev>
2026-03-13 20:31:16 -0600

    feat: Add commands for cloning the hydenix repository and performing an initial NixOS installation.

--- comando-maestro-para-rebuild.sh
+++ comando-maestro-para-rebuild.sh
@@ -1,1 +1,6 @@
 sudo nixos-rebuild switch --flake /home/ravn/Work/hydenix#default
+
+
+git clone https://github.com/richen604/hydenix /mnt/etc/nixos
+# Instalar
+sudo nixos-install --flake /mnt/etc/nixos#default
